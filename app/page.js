5b986945b77d1246a814e8d8e9c46f4f8ab5936d
Francisco Lucena <franciscolucena90@gmail.com>
2023-08-17 18:26:35 -0300

    Initial commit v2

--- app/page.js
+++ app/page.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import Tetris from './components/Tetris'
+import React from 'react';
+import Tetris from './components/Tetris';
+
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  marginLeft: '5vw',
+};
+
+const titleStyle = {
+  flexGrow: 1, // Allow title to take available space
+};
 
 const App = () => {
-  return(
+  return (
     <div className='App'>
-      <Tetris />
+      <div style={containerStyle}>
+        <div style={titleStyle}>
+          <h1>Tetris</h1>
+          <span>By FL Automations</span>
+        </div>
+        <Tetris />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App; 
+export default App;
